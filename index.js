b4c39343deeb48c48a2e66eda7a2769681522a4d
Atticus-Robinson <acerobinson2@gmail.com>
2022-04-24 11:18:45 -0500

    added inquirer prompts

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const inquirer = require("inquirer");
 const { Department, Role, Employee } = require("./utils/constructors.js");
 
-const jim  = new Employee('Jim', 'Henson', 2, 2);
-
-console.log(jim);
+inquirer
+  .prompt([
+    {
+      name: "initialQ",
+      message: "What would you like to do...",
+      type: "list",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "View employees by manager",
+        "View employees by department",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update and employee role",
+        "Update employee manager",
+        "Delete a department",
+        "Delete a role",
+        "Delete an employee",
+      ],
+    },
+  ])
+  .then((answers) => {
+      console.log(answers.initialQ)
+  })
+  .catch((error) => {
+      console.log(error);
+  });
