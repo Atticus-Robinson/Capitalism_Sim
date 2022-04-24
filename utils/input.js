a8b4d32ba0169cc0a752cd4c97a381c61b840b39
Atticus-Robinson <acerobinson2@gmail.com>
2022-04-24 12:57:53 -0500

    changed file structure

--- utils/input.js
+++ utils/input.js
@@ -1,34 +1,15 @@
 const inquirer = require("inquirer");
 const switchboard = require("./switchboard");
-const { viewDepartments } = require("./manip");
+const firstQ = require("../lib/questions");
 
 
 function initialQ() {
     inquirer
-    .prompt([
-      {
-        name: "initialQ",
-        message: "What would you like to do...",
-        type: "list",
-        choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "View employees by manager",
-          "View employees by department",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update and employee role",
-          "Update employee manager",
-          "Delete a department",
-          "Delete a role",
-          "Delete an employee",
-        ],
-      },
-    ])
+    .prompt(firstQ
+    )
     .then((answer) => {
         switchboard(answer.initialQ);
+        initialQ();
     })
     .catch((error) => {
         console.log(error);
