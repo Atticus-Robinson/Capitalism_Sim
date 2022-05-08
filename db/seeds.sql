f5a9697ff4ce508bf5428f7f72a3fef7178b9311
Atticus-Robinson <acerobinson2@gmail.com>
2022-05-08 17:28:33 -0500

    view employees working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,30 @@
-INSERT INTO department (name) 
+INSERT INTO departments (name) 
 VALUES  
-    ('HR'),
-    ('Engineering'),
     ('Sales'),
-    ('Accounting');
+    ('Accounting'),
+    ('HR'),
+    ('Operations'),
+    ('Management');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ('Network Engineer', 80000, 2);
+    ('Salesman', 80000, 1),
+    ('Accountant', 90000, 2),
+    ('HR Rep', 70000, 3),
+    ('District Manager', 100000, 5);
 
-INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Jim', 'Henson', 1, 1);
+    ('Michael', 'Scott', 4, NULL),
+    ('Pam', 'Beesly', 2, 1),
+    ('Jim', 'Halpert', 1, 1),
+    ('Dwight', 'Schrute', 1, 3),
+    ('Philis', 'Vance', 1, 3),
+    ('Stanley', 'Hudson', 1, 3),
+    ('Andy', 'Bernard', 1, 3),
+    ('Oscar', 'Nunez', 2, 1),
+    ('Angela', 'Martin', 2, 8),
+    ('Kevin', 'Malone', 2, 8),
+    ('Kelly', 'Kapoor', 3, 1),
+    ('Toby', 'Flenderson', 3, 1);
