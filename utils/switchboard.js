8c078b80924b65748609ec4e9641c2632144aa1d
Atticus-Robinson <acerobinson2@gmail.com>
2022-05-08 14:43:00 -0500

    beggining SQL entries

--- utils/switchboard.js
+++ utils/switchboard.js
@@ -2,9 +2,9 @@ const start = require("../lib/questions");
 const inquirer = require("inquirer");
 
 //Functions
-const viewDepartments = require("../lib/deparment");
-// const {} = require("../lib/employee");
-// const {} = require("../lib/role");
+const { viewDepartments } = require("../lib/deparment");
+const { viewEmployees, viewByManager } = require("../lib/employee");
+const { viewRoles } = require("../lib/role");
 
 const switchboard = async (choice) => {
   switch (choice) {
@@ -14,11 +14,19 @@ const switchboard = async (choice) => {
     case "View all departments":
       await viewDepartments();
       break;
+    case "View all roles":
+      await viewRoles();
+      break;
+    case "View all employees":
+      await viewEmployees();
+      break;
+    case "View employees by manager":
+      await viewByManager();
+      break;
     default:
       exit();
       break;
   }
-  // await again();
 };
 
 module.exports = switchboard;
