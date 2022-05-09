45a7f278edbd84610fa0145531bc98e26860ccfa
Atticus-Robinson <acerobinson2@gmail.com>
2022-05-08 22:33:36 -0500

    working on roles sql

--- utils/input.js
+++ utils/input.js
@@ -1,14 +1,16 @@
 //Question library
-const start = require("../lib/questions");
+const { start, addDepartmentQ, addRoleQ } = require("../lib/questions");
 
-//Switchborad util
-const switchboard = require("./switchboard");
-
-const inquirer = require("inquirer");
 const db = require("../db/connection");
+const inquirer = require("inquirer");
+
+//SQL Functions
+const { viewDepartments, addDepartment } = require("../lib/deparment");
+const { viewEmployees, viewByManager } = require("../lib/employee");
+const { viewRoles, addRole } = require("../lib/role");
 
 const initiateProgram = async () => {
-   inquirer
+  inquirer
     .prompt(start)
     .then(async (answer) => {
       await switchboard(answer.start);
@@ -19,15 +21,66 @@ const initiateProgram = async () => {
     });
 };
 
-const again = async () => {
- initiateProgram();
+const addDepartmentAsk = async () => {
+  await inquirer
+    .prompt(addDepartmentQ)
+    .then(async (answer) => {
+      await addDepartment(answer.addDepartmentQ);
+      again();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-function exit() {
-  console.log("exit");
-}
+const addRoleAsk = async () => {
+  let currentDepartments = getAllDepartments();
+  await inquirer
+    .prompt(addRoleQ)
+    .then(async (answer) => {
+      await addDepartment(answer.addRoleQ);
+      again();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const again = async () => {
+  initiateProgram();
+};
+
+const switchboard = async (choice) => {
+  switch (choice) {
+    case "Exit":
+      exit();
+      break;
+    case "View all departments":
+      await viewDepartments();
+      break;
+    case "View all roles":
+      await viewRoles();
+      break;
+    case "View all employees":
+      await viewEmployees();
+      break;
+    case "View employees by manager":
+      await viewByManager();
+      break;
+    case "Add a department":
+      await addDepartmentAsk();
+      break;
+    case "Add a role":
+      await addRoleAsk();
+      break;
+    default:
+      exit();
+      break;
+  }
+};
 
 module.exports = {
   initiateProgram,
   again,
+  addDepartmentAsk,
 };
