aed42c830e4531a8a62328fe7379487479952634
Atticus-Robinson <acerobinson2@gmail.com>
2022-05-10 20:55:02 -0500

    finished add role

--- utils/input.js
+++ utils/input.js
@@ -55,7 +55,7 @@ const switchboard = async (choice) => {
       await addDepartment();
       break;
     case "Add a role":
-      await addRoleAsk();
+      await addRole();
       break;
     default:
       exit();
